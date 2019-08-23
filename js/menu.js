d39b95094ae623ec6bcbb00804a47560167711c0
unknown <timedge5@gmail.com>
2019-08-22 19:49:35 -0700

    mq finish, popdown menu added

--- js/menu.js
+++ js/menu.js
@@ -1,31 +1,41 @@
 //declaring variables to select DOM items in the HTML - using const as these are going to be constants
-const menubtn = document.querySelector('.menu-btn');
-const menu = document.querySelector('.menu');
-const close = document.querySelector('.close-btn');
-const navitems = document.querySelectorAll('.nav-item');
+// const menubtn = document.querySelector('.menu-btn');
+// const menubtn1 = document.querySelector('.hide');
+// const menu = document.querySelector('.menu');
+// // const close = document.querySelector('.close-btn');
+// const navitems = document.querySelectorAll('.nav-item');
 
-let showMenu = false;
+// let showMenu = false;
 
-menubtn.addEventListener('click', toggleMenu);
-close.addEventListener('click', toggleMenu);
+// menubtn.addEventListener('click', toggleMenu);
+// menubtn1.addEventListener('click', toggleMenu);
+// // close.addEventListener('click', toggleMenu);
 
-for(i = 0; i < navitems.length; i++) {
-  result = navitems[i];
-  result.addEventListener('click', toggleMenu);
-}
+// for(i = 0; i < navitems.length; i++) {
+//   result = navitems[i];
+//   result.addEventListener('click', toggleMenu);
+// }
 
-function toggleMenu() {
-  if (!showMenu) {
-    menubtn.classList.add('hide');
-    menu.classList.add('show');
-    close.classList.add('show');
+// function toggleMenu() {
+//   if (!showMenu) {
+//     menubtn.classList.add('hide');
+//     menu.classList.add('show');
+//     close.classList.add('show');
 
-    showMenu = true;
-  } else {
-    menubtn.classList.remove('hide');
-    menu.classList.remove('show');
-    close.classList.remove('show');
+//     showMenu = true;
+//   } else {
+//     menubtn.classList.remove('hide');
+//     menu.classList.remove('show');
+//     close.classList.remove('show');
     
-    showMenu = false;
-  }
-}
+//     showMenu = false;
+//   }
+// }
+
+
+$(document).ready(()=> {
+  $('span.menu-btn').click(() => {
+    $('span').toggleClass('hide');
+    $('div.menu').toggleClass('show');
+  });
+});
